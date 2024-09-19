e8f16941c177b1b6efcfd41f7ceb564d2a5d944a
aaaaaahan <jookh-am20@student.tarc.edu.my>
2024-09-19 15:03:46 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.crlg02pwc9wg.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "jookaihan-employee"
+customregion = "us-east-1a"
+
