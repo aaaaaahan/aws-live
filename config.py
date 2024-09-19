bf4ed1298f2f44c3db8bb5882668f739f0c83dec
aaaaaahan <jookh-am20@student.tarc.edu.my>
2024-09-19 15:03:09 +0800

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-customhost = "employee.crlg02pwc9wg.us-east-1.rds.amazonaws.com"
-customuser = "aws_user"
-custompass = "Bait3273"
-customdb = "employee"
-custombucket = "jookaihan-employee"
-customregion = "us-east-1a"
-
