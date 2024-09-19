75c0eebef0344a36df2926bc7cb86687ae090f99
aaaaaahan <jookh-am20@student.tarc.edu.my>
2024-09-19 15:00:15 +0800

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.chauwtk5goof.us-east-1.rds.amazonaws.com"
+customhost = "employee.crlg02pwc9wg.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
-customregion = "us-east-1"
+custombucket = "jookaihan-employee"
+customregion = "us-east-1a"
 
